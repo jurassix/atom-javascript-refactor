6fddc2b7cf2678b2eda20362318963235a94f9a4
Clinton Ayres <clinton.ayres@theice.com>
2016-06-26 12:34:39 -0400

    init

---
+++ spec/atom-refactoring-codemods-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PathRenameForm from '../lib/PathRenameForm';
+
+describe('PathRenameForm', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
